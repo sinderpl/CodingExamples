d19e41bdf198bf11a6c81504301c5c31a307039a
sinderpl <sinderpl@gmail.com>
2017-07-24 15:21:10 +0100

    FizzBuzz is now a class

--- python/FizzBuzz/FizzBuzz.py
+++ python/FizzBuzz/FizzBuzz.py
@@ -1,22 +1,30 @@
-print("Welcome to FizzBuzz");
+class FizzBuzz:
+    print("Welcome to FizzBuzz");
 
-# Amount of numbers to count to
-size = 100;
+    def __init__(self):
+        # Amount of numbers to count to
+        self.size = 100;
 
-#Count from 1 to 100 inclusive
-for num in range(1, size+1):
-    #If the number is a multiple of 3 and 5
-    #print the Fizzbuzz keyword
-    if(num%5 is 0 and num%3 is 0):
-        print("FizzBuzz")
-    #If the number is a multiple of 3
-    #Print Fizz
-    elif(num%3 is 0):
-        print("Fizz")
-    #If the numebr is a multiple of 5
-    #Print the Buzz keyword
-    elif(num%5 is 0):
-        print("Buzz")
-    #Otherwise just print the number
-    else:
-        print(num)
+
+
+    def print_fizzBuzz(self):
+        #Count from 1 to 100 inclusive
+        for num in range(1, self.size+1):
+            #If the number is a multiple of 3 and 5
+            #print the Fizzbuzz keyword
+            if(num%5 is 0 and num%3 is 0):
+                print("FizzBuzz")
+            #If the number is a multiple of 3
+            #Print Fizz
+            elif(num%3 is 0):
+                print("Fizz")
+            #If the numebr is a multiple of 5
+            #Print the Buzz keyword
+            elif(num%5 is 0):
+                print("Buzz")
+            #Otherwise just print the number
+            else:
+                print(num)
+
+x = FizzBuzz()
+x.print_fizzBuzz()
