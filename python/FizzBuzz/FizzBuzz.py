132f51e9ea9209aa9d6ace95321bd3f0f3af4c38
sinderpl <sinderpl@gmail.com>
2017-07-24 16:10:18 +0100

    Python code and test now completed

--- python/FizzBuzz/FizzBuzz.py
+++ python/FizzBuzz/FizzBuzz.py
@@ -1,30 +1,38 @@
 class FizzBuzz:
-    print("Welcome to FizzBuzz");
 
     def __init__(self):
-        # Amount of numbers to count to
-        self.size = 100;
+        self.output = []
 
 
 
-    def print_fizzBuzz(self):
+
+    def calculate_fizzBuzz(self, size):
+
         #Count from 1 to 100 inclusive
-        for num in range(1, self.size+1):
+        for num in range(1, size+1):
             #If the number is a multiple of 3 and 5
             #print the Fizzbuzz keyword
             if(num%5 is 0 and num%3 is 0):
-                print("FizzBuzz")
+                self.output.append("FizzBuzz")
             #If the number is a multiple of 3
             #Print Fizz
             elif(num%3 is 0):
-                print("Fizz")
+                self.output.append("Fizz")
             #If the numebr is a multiple of 5
             #Print the Buzz keyword
             elif(num%5 is 0):
-                print("Buzz")
+                self.output.append("Buzz")
             #Otherwise just print the number
             else:
-                print(num)
+                self.output.append(num)
 
-x = FizzBuzz()
-x.print_fizzBuzz()
+    def print_fizzBuzz(self):
+        for element in self.output:
+            print element
+
+
+if __name__ == '__main__':
+    print("Welcome to FizzBuzz");
+    x = FizzBuzz()
+    x.calculate_fizzBuzz(100)
+    x.print_fizzBuzz()
--- python/FizzBuzz/FizzBuzzTest.py
+++ python/FizzBuzz/FizzBuzzTest.py
@@ -1,5 +1,24 @@
 import unittest
+from FizzBuzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_output
+    def test_output(self):
+        #Set up test size
+        #In this case 15 because it allows to check all possibilites
+        # "Fizz,Buzz , FizzBuzz and a normal number"
+        testSize = 15
+
+        #Set up the test class and its calculations
+        fizzBuzz = FizzBuzz()
+        fizzBuzz.calculate_fizzBuzz(testSize)
+
+        output = fizzBuzz.output
+
+        self.assertEqual(output[0], 1, 'The predicted number was incorrect.')
+        self.assertEqual(output[2], 'Fizz', 'The predicted output was incorrect.')
+        self.assertEqual(output[4], 'Buzz', 'The predicted output was incorrect.')
+        self.assertEqual(output[14], 'FizzBuzz', 'The predicted output was incorrect.')
+
+if __name__ == '__main__':
+    unittest.main()
