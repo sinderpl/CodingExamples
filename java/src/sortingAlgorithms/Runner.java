c9e793a8c73b8ecd692349f17fa114bb2a025102
sinderpl <18658841+sinderpl@users.noreply.github.com>
2017-08-14 17:27:39 +0100

    Two sorted integer arrays can now be merged into a single sorted one

--- java/src/sortingAlgorithms/Runner.java
+++ java/src/sortingAlgorithms/Runner.java
@@ -10,14 +10,23 @@ public class Runner {
 	public static void main(String[] args) {
 		mergeSort merge = new mergeSort();
 		
-		ArrayList<Integer> arr = new ArrayList<Integer>();
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		ArrayList<Integer> arr2 = new ArrayList<Integer>();
 		
-		arr.add(1);
-		arr.add(2);
-		arr.add(8);
-		arr.add(15);
+		arr1.add(1);
+		arr1.add(8);
+		arr1.add(2);
+		arr1.add(15);
 		
-		System.out.println(merge.isSorted(arr));
+		arr2.add(3);
+		arr2.add(4);
+		arr2.add(6);
+		arr2.add(9);
+		
+		System.out.println(merge.isSorted(arr1));
+		System.out.println(merge.isSorted(arr2));
+		
+		System.out.println(merge.mergeArrays(arr1, arr2));
 
 	}
 
--- java/src/sortingAlgorithms/mergeSort.java
+++ java/src/sortingAlgorithms/mergeSort.java
@@ -12,14 +12,38 @@ import java.util.ArrayList;
  */
 public class mergeSort extends sortingAlgorithm {
 
+	/**
+	 * Combines two sorted integer arrays into a single sorted array
+	 * @param first Sorted integer array number one
+	 * @param second Sorted interger array number two
+	 * @return A sorted array combining the two arrays
+	 */
 	public ArrayList<Integer> mergeArrays(ArrayList<Integer> first, ArrayList<Integer> second){
 		//Initialise the output array
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		//Boolean check  whether the array is sorted
 		boolean isSorted = false;
-		while (!isSorted){
-			
+		
+		int left = 0;
+		int right = 0;
+		
+		int totalSize = first.size() + second.size();
+		totalSize -= 2;
+		
+		for(int x= totalSize; x >=0 ; x--){
+			if(first.get(left) > second.get(right)){
+				result.add(second.get(right));
+				right++ ;
+			}
+			else{
+				result.add(first.get(left));
+				left++;
+			}
 		}
+		
+		System.out.println("New array is sorted: " + isSorted(result));
+		
+		
 		//Return the output
 		return result;
 	}
