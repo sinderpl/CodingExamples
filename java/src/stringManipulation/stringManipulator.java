be7de89d022affabbc5a38ada917ec0398d1ec07
sinderpl <18658841+sinderpl@users.noreply.github.com>
2017-08-14 14:15:06 +0100

    Separate split() method created for splitting input into words

--- java/src/stringManipulation/stringManipulator.java
+++ java/src/stringManipulation/stringManipulator.java
@@ -66,19 +66,71 @@ public class stringManipulator {
 		System.out.printf("WordCount: %d , LetterCount: %d \n", wordCount, letterCount);
 		//Calculate the average amount of letter per word
 		float average = (float) letterCount/wordCount;
+		//Print the average
 		System.out.printf("Average letter count per word: %d / %d = %.2f", letterCount, wordCount, average);
 	}
 	
+	/**
+	 * 
+	 * Splits the input string into individual words without using the java string split method
+	 * 
+	 * @param string
+	 * @return A String array with the individual words split from input string
+	 */
+	
+	public ArrayList<String> split(String string){
+		//Variables
+		ArrayList<String> stringArray = new ArrayList<String>();
+		boolean isCurrentWord = false;
+		String currentWord = "";
+		
+		//Convert the string to a char array to allow for finding alphabet characters
+		char[] charArray = string.toCharArray();
+		
+		/**
+		 * Iterate through each character picking out individual words
+		 * when hitting anything that is not a alphabetic character
+		 */
+		for(int x=0; x<string.length(); x++){
+			//Checks if the char is a letter
+			if(Character.isLetter(charArray[x])){
+				//Start the creation of a word
+				isCurrentWord = true;
+				//Add the character 
+				currentWord += charArray[x];
+			}else{
+				//The word was finished so we set the current word flag to false
+				isCurrentWord = false;
+				//Adds the currentWord to the word array if it 
+				if(currentWord != ""){
+					stringArray.add(currentWord);
+				// Set the currentWord to empty again
+					currentWord = "";
+				} //End if
+			}//End else
+		} //End For loop
+		
+		/**
+		 * Do one final check as the code can omit final words in some cases
+		 * This happens if the final word has no following character or empty space
+		 */
+		if(isCurrentWord && currentWord != ""){
+			stringArray.add(currentWord);
+				currentWord = "";
+			}// End if
+		//Return the output
+		return stringArray;
+	}//End Method
 	
 
 	/**
-	 * @param args
+	 *
 	 */
 	public static void main(String[] args) {
 		stringManipulator manipulator = new stringManipulator();
 		
-		manipulator.countAverageLetters(" I am currently interviewing at SAP ");
-		
+		//manipulator.countAverageLetters(" I am currently interviewing at SAP ");
+		System.out.println(manipulator.split(" I am currently, interviewing at SAP"));
 
 	}
 
