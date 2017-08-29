5e0cc2ba0673f4305716e6449dbb0d5e5bb29b9c
sinderpl <18658841+sinderpl@users.noreply.github.com>
2017-08-29 14:46:26 +0100

    Time complexity checking for string reversal StringBuilder vs My for loop

--- java/src/stringManipulation/runner.java
+++ java/src/stringManipulation/runner.java
@@ -18,7 +18,28 @@ public class runner {
 		
 		stringManipulator manipulator = new stringManipulator();
 		
-		System.out.println(manipulator.reverse("trouble"));
+		long startTime1 = System.nanoTime();
+		//My method
+		manipulator.reverse("trouble");
+		long endTime1 = System.nanoTime();
+		
+		
+		long startTime2 = System.nanoTime();
+		//String builder
+		manipulator.reverseBuilder("trouble");
+		long endTime2 = System.nanoTime();
+		
+		long execTime1 = endTime1 - startTime1;
+		long execTime2 = endTime2 - startTime2;
+		
+		System.out.printf("For loop time: %d%n   || StringBuilder time: %d%n \n" , execTime1, execTime2);
+		if(execTime1 < execTime2){
+			System.out.println("For Loop wins");
+		}else{
+			System.out.println("String builder wins");
+		}
+		
+		
 		manipulator.countAverageLetters(testString);
 		manipulator.reverse(testString);
 		manipulator.reverseWords(testString);
