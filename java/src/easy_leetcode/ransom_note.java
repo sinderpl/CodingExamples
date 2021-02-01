a8dc63a433b072650aadbd21c639e9d3f370efd5
Alan <sinderpl@gmail.com>
2021-02-01 15:04:28 +0000

    ransom note

--- java/src/easy_leetcode/ransom_note.java
+++ java/src/easy_leetcode/ransom_note.java
@@ -46,3 +46,26 @@ class Solution {
         
     }
 }
+
+/**
+ * Persibak faster solution utilising a simple hashtable instead ( assumes only a standard 26 character alphabet)
+ */
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()){return false;}
+        int[] alpha = new int[26];
+        for(char c : magazine.toCharArray()){
+            alpha[c - 'a'] = alpha[c - 'a'] + 1;
+        }
+        
+        for(char c : ransomNote.toCharArray()){
+            if(alpha[c - 'a'] > 0){
+                alpha[c - 'a'] = alpha[c - 'a'] -1;
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
