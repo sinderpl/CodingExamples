e7c0a820be284e7f6a87d5e2f5d46f179360b248
Alan <sinderpl@gmail.com>
2021-02-02 21:13:07 +0000

    student attendance recored

---
+++ java/src/easy_leetcode/student_attendance_record.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ * 551. Student Attendance Record I
+Easy
+
+360
+
+911
+
+Add to List
+
+Share
+You are given a string representing an attendance record for a student. The record only contains the following three characters:
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+Example 2:
+Input: "PPALLL"
+Output: False
+ */
+
+
+class Solution {
+    public boolean checkRecord(String s) {
+        boolean isA = false;
+        int lCount = 0;
+        
+        for(char c : s.toCharArray()){
+            if(c == 'A'){
+                if(isA){
+                    return false;
+                } else{
+                    isA = true;
+                }
+            } else if( c == 'L'){
+                lCount++;
+                if(lCount > 2){
+                    return false;
+                }
+            }
+            if( c != 'L'){
+                lCount = 0;
+            }
+        }
+        
+        return true;
+    }
+}
