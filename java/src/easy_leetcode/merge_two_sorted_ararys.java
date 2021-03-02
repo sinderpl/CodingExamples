b448e96a484aaeee7aa6364facf13f91cc7436c0
Alan <sinderpl@gmail.com>
2021-03-02 15:08:27 +0000

    shorter way

--- java/src/easy_leetcode/merge_two_sorted_ararys.java
+++ java/src/easy_leetcode/merge_two_sorted_ararys.java
@@ -44,4 +44,24 @@ class Solution {
         }
         
     }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0){
+            return;
+        }
+        
+        int end = m + n -1;
+        m--; n--;
+        
+        while(m >= 0 && n >= 0){
+            if(nums1[m] > nums2[n])
+                nums1[end--] = nums1[m--];
+            else
+                nums1[end--] = nums2[n--];
+        }
+        
+        while(n >= 0)
+            nums1[end--] = nums2[n--];
+    }
+}
 }
