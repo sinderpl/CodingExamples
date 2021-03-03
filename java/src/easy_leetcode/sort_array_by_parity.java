1774a933968caa270f972053ae43919d95c7eaf4
Alan <sinderpl@gmail.com>
2021-03-03 20:33:58 +0000

    sort array by parity

---
+++ java/src/easy_leetcode/sort_array_by_parity.java
@@ -0,0 +1,23 @@
+/**
+ * Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+You may return any answer array that satisfies this condition.
+ */
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int i = 0, j = A.length - 1;
+        while (i < j) {
+            if (A[i] % 2 > A[j] % 2) {
+                int tmp = A[i];
+                A[i] = A[j];
+                A[j] = tmp;
+            }
+
+            if (A[i] % 2 == 0) i++;
+            if (A[j] % 2 == 1) j--;
+        }
+
+        return A;
+    }
+}
