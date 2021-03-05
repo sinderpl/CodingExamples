f94ff4a5a0ab86ef67cd70e56690082ebe4f7f5a
Alan <sinderpl@gmail.com>
2021-03-05 21:58:29 +0000

    shorten

--- java/src/leetcode_linked_lists/easy/easy/linked_list_cycle.java
+++ java/src/leetcode_linked_lists/easy/easy/linked_list_cycle.java
@@ -28,16 +28,12 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null){
-            if(fast.next != null && fast.next.next != null)
-                fast = fast.next.next;
-            else 
-                return false;
+        while(fast.next != null && fast.next.next != null){
             slow = slow.next;
+            fast = fast.next.next;
             if(slow == fast)
                 return true;
         }
-        
         return false;
     }
 } 
