bfe3da13431297cb2a669475d66abd29770a4367
Alan <sinderpl@gmail.com>
2021-03-31 21:19:53 +0100

    smartest solution

--- java/src/leetcode_linked_lists/easy/easy/IntersectionTwoLinkedLists.java
+++ java/src/leetcode_linked_lists/easy/easy/IntersectionTwoLinkedLists.java
@@ -116,3 +116,27 @@ public class Solution {
         return null;
     }
 }
+
+//Fastest
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while(pA != pB){
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
