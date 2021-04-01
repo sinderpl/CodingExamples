7c96a5fc0a911686c6bfe104b1e8d4c89e8169d0
Alan <sinderpl@gmail.com>
2021-04-01 18:30:23 +0100

    remove_node

---
+++ java/src/leetcode_linked_lists/easy/easy/remove_node_at_n.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ * 
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Follow up: Could you do this in one pass?
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+ */
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head.next == null) return head;
+        ListNode nLast = head;
+        ListNode currNode = head;
+        for (int i = 0; i < n; i++){
+            currNode = currNode.next;
+        }
+        
+        while(currNode.next != null){
+            nLast = nLast.next;
+            currNode = currNode.next;
+        }
+        
+        nLast.next = currNode;
+            
+        return head;
+    }
+}
