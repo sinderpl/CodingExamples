20cd537265754b1aa6aa52404771570715803047
Alan <sinderpl@gmail.com>
2021-04-01 18:47:53 +0100

    cleanup

--- java/src/leetcode_linked_lists/easy/easy/remove_node_at_n.java
+++ java/src/leetcode_linked_lists/easy/easy/remove_node_at_n.java
@@ -79,7 +79,7 @@ class Solution {
         ListNode first = dummy;
         ListNode second = dummy;
         // Advances first pointer so that the gap between first and second is n nodes apart
-        for (int i = 1; i <= n + 1; i++) {
+        for (int i = 0; i <= n; i++) {
             first = first.next;
         }
         // Move first to the end, maintaining the gap
