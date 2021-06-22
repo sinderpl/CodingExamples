f6df469b58d9d697aef7767fc3925861ca934965
Alan <sinderpl@gmail.com>
2021-06-22 14:58:08 +0100

    Saerch binary tree

--- python/Data Structures/Binary Search Tree/SearchBinaryTree.py
+++ python/Data Structures/Binary Search Tree/SearchBinaryTree.py
@@ -6,10 +6,10 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
-        if root is None or root == val:
+        if root is None or root.val == val:
             return root
         
         if root.val < val:
             return self.searchBST(root.left, val)
         return self.searchBST(root.right, val)
-        
+
