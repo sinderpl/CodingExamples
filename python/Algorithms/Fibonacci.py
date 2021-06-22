d606a887e606a4da5a5dbf0012dbbefc77b58d94
Alan <sinderpl@gmail.com>
2021-06-22 16:11:25 +0100

    fibonacci

---
+++ python/Algorithms/Fibonacci.py
@@ -0,0 +1,44 @@
+
+
+#Recursion
+class Solution:
+    def fib(self, n: int) -> int:
+        if (n <= 1) :
+            return n 
+        return self.fib(n-1) + self.fib(n-2)
+
+#Bottom up memoization
+    def fib(self, n: int) -> int:
+        if (n <= 1):
+            return n
+        
+        cache = [0,1]
+        for i in range(2,n+1):
+            cache.append(cache[i-1] + cache[i-2])
+            
+        return cache[n]
+        
+#Top down memoization
+ #SAme as above but you precompute the N and store as a global
+
+
+#Top Down Iterative
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        elif n == 2:
+            return 1
+
+        prev1 = 1
+        prev2 = 1
+        curr = 0
+
+        for i in range(3, n+1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        return curr
+
+    
+    
+        
