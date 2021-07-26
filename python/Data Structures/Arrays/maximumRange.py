e99df3a35d542096dec60384550de23ea75106c7
Alan <sinderpl@gmail.com>
2021-07-26 09:56:39 +0100

    max year

--- python/Data Structures/Arrays/maximumRange.py
+++ python/Data Structures/Arrays/maximumRange.py
@@ -3,6 +3,8 @@ class Solution:
         
         years_mapped = dict()
         # Map each person = O(n)
+        # First bottleneck is having to map every year out which increases processing time
+        # The second is also here since we use additional storage to account for all the years
         for years in logs:
             currYear = years[0]
             
@@ -21,4 +23,33 @@ class Solution:
                 max_year_count = count
         # O(n* (death- currYear)) ??
         return max_year
+        
+
+#Hashmap is not really that much more efficient
+#The runtime was slightly faster most likely due to the speedier lookup than dictionairy
+# Not much of a space improvement either
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        
+        years_mapped = [0] * 100
+        print(years_mapped)
+        # Map each person = O(n)
+        for years in logs:
+            currYear = years[0]
+            # O(n* (death- currYear))
+            while currYear < years[1]:
+                years_mapped[currYear - 1950] += 1
+                currYear += 1
+        
+        max_year = 0
+        max_year_count = 0
+        for year, count in enumerate(years_mapped):
+            if count > max_year_count:
+                max_year = year
+                max_year_count = count
+            elif count == max_year_count and year < max_year:
+                max_year = year
+                max_year_count = count
+        
+        return 1950 + max_year
         
