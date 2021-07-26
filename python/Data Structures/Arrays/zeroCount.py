5f46f09942a1781129ad1d795731c376bdba41f1
Alan <sinderpl@gmail.com>
2021-07-26 13:30:07 +0100

    duplicate zeros

--- python/Data Structures/Arrays/zeroCount.py
+++ python/Data Structures/Arrays/zeroCount.py
@@ -25,5 +25,31 @@ class Solution:
             write -= 1
         print(zeroCount)
         
+
+#Uses additional space complexity that is not neeeded
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
         
+        cp = arr.copy()
+        curr = 0
+        
+        for idx, num in enumerate(cp):
+            if curr >= len(arr):
+                break
+                
+            arr[curr] = num
+            curr += 1
+            if num == 0 and curr < len(arr):
+                arr[curr] = 0
+                curr += 1
+            
+            
+            
+            
+                
+        
+           
         
