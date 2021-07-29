013a41060264a9e6f6beaa64117d1e0fdf3a8eed
Alan <sinderpl@gmail.com>
2021-07-29 08:46:13 +0100

    third max

--- python/Data Structures/Arrays/thirdMaximum.py
+++ python/Data Structures/Arrays/thirdMaximum.py
@@ -22,3 +22,27 @@ class Solution:
                 third = num
                 
         return third if third != -sys.maxsize else first
+
+
+    def thirdMax(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        if len(nums) < 3:
+            return nums[0] if nums[0] > nums[1] else nums[1]
+        
+        first = nums[0]
+        second = -sys.maxsize
+        third = -sys.maxsize
+        
+        for idx in range(1, len(nums)):
+            if nums[idx] > first:
+                third = second
+                second = first
+                first = nums[idx]
+            elif nums[idx] > second:
+                third = second
+                second = nums[idx]
+            elif nums[idx] > third:
+                third = nums[idx]
+                
+        return third if third != -sys.maxsize else first
