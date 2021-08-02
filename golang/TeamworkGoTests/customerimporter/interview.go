08a279956dcd6eaac4e00f5f0ee7fa3c818343b3
Alan <sinderpl@gmail.com>
2021-08-02 14:24:25 +0100

    finished tests

--- golang/TeamworkGoTests/customerimporter/interview.go
+++ golang/TeamworkGoTests/customerimporter/interview.go
@@ -17,24 +17,14 @@ import (
 const (
 	fileNotOpened = "The selected file could not be opened"
 	fileNotRead   = "The selected file could not be read correctly"
-	emailNotFound = "The email could not be found: "
+	emailNotFound = "The email could not be found at cell: "
 )
 
-type custData struct {
-	Name   string
-	Email  string
-	Gender string
-	IP     string
-}
-
 // Parse file content and validate for any errors
-func ParseFileContent(fileContent [][]string) map[string]int {
-
+func FilterCustomerDomains(fileContent [][]string) map[string]int {
 	customerDomains := make(map[string]int)
-	for _, line := range fileContent[1 : len(fileContent)-2] {
-		// custEmail := line[2]
+	for _, line := range fileContent[1:len(fileContent)] {
 		custDomain, err := getCustomerDomain(line)
-
 		if err != nil {
 			fmt.Println(err)
 		} else {
@@ -45,9 +35,14 @@ func ParseFileContent(fileContent [][]string) map[string]int {
 }
 
 func getCustomerDomain(line []string) (string, error) {
+	if len(line) <= 2 {
+		return "", errors.New(emailNotFound)
+	}
 	email, err := mail.ParseAddress(line[2])
 	if err != nil {
-		return "", err
+		// Could attempt to parse the email ourselves here since the checker could be limited
+		// to a strict set of rules
+		return "", errors.New(emailNotFound + line[2])
 	}
 
 	lastAt := strings.LastIndex(email.Address, "@")
@@ -60,23 +55,27 @@ func getCustomerDomain(line []string) (string, error) {
 
 // Reads in the specified file and returns the contents
 func ReadFileContents(fileName string) ([][]string, error) {
-
 	csvFile, err := os.Open(fileName)
 	if err != nil {
 		return nil, errors.New(fileNotOpened)
 	}
-
 	defer csvFile.Close()
 
 	csvLines, err := csv.NewReader(csvFile).ReadAll()
 	if err != nil {
 		return nil, errors.New(fileNotRead)
 	}
-
 	return csvLines, nil
 }
 
 //Unrelated
+type custData struct {
+	Name   string
+	Email  string
+	Gender string
+	IP     string
+}
+
 func printCustomers(fileContent [][]string) {
 	for _, line := range fileContent[1:] {
 		cust := custData{
--- golang/TeamworkGoTests/customerimporter/interview_test.go
+++ golang/TeamworkGoTests/customerimporter/interview_test.go
@@ -1,1 +1,69 @@
 package customerimporter
+
+import (
+	"log"
+	"testing"
+)
+
+func TestCustomerDomainSuccess(t *testing.T) {
+
+	failureScenarios := [][]string{
+		{"", "", "email@example.co.jp", ""},
+		{"", "", "firstname-lastname@example.com", ""},
+		{"", "", "_______@example.com", ""},
+		{"", "", "email@123.123.123.123", ""},
+		{"", "", "email@example.name", ""},
+	}
+
+	for _, line := range failureScenarios {
+		_, err := getCustomerDomain(line)
+		if err != nil {
+			t.Fail()
+			t.Logf("Expected the customer parsing to succeed, a error has occured instead %s ", line)
+			log.Fatal(err)
+		}
+	}
+}
+
+func TestCustomerDomainFailures(t *testing.T) {
+
+	failureScenarios := [][]string{
+		{"", "", "email", ""},
+		{"", "", "84575,", ""},
+		{"", "", "@@", ""},
+		{"", "", "dfd@.com", ""},
+		{"", "", "", ""},
+		{"", ""},
+		{"", "", "------@", ""},
+	}
+
+	for _, line := range failureScenarios {
+		_, err := getCustomerDomain(line)
+		if err == nil {
+			t.Fail()
+			t.Logf("Expected the customer parsing read to fail, a error has not been thrown %s ", line)
+			log.Fatal(err)
+		}
+	}
+}
+
+func TestReadFileExists(t *testing.T) {
+	fileName := "../data/customers.csv"
+	_, err := ReadFileContents(fileName)
+
+	if err != nil {
+		t.Fail()
+		t.Logf("Expected the file read to succed, a error has occured instead %s", fileName)
+		log.Fatal(err)
+	}
+}
+
+func TestReadFileDoesNotExist(t *testing.T) {
+	fileName := "../data/.csv"
+	_, err := ReadFileContents(fileName)
+	if err == nil {
+		t.Fail()
+		t.Logf("Expected the file read to fail, the error should not be empty %s", fileName)
+		log.Fatal(err)
+	}
+}
