6c17bded26aa21d841848682ff3d013cf8002e4b
Alan <sinderpl@gmail.com>
2021-08-18 12:34:54 +0100

    another approach

--- python/Data Structures/Linked Lists/removeValLinkedList.py
+++ python/Data Structures/Linked Lists/removeValLinkedList.py
@@ -21,4 +21,28 @@ class Solution:
         
         return newHead
         
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        if head == None or (head.val == val and head.next == None) :
+            return None
+        
+        ptr1 = head
+        ptr2 = head.next
+        
+        while ptr2 != None:
+            if ptr2.val == val:
+                ptr1.next = ptr2.next
+                ptr2 = ptr2.next
+            else:
+                ptr1 = ptr2
+                ptr2 = ptr2.next
+        
+        return head if head.val != val else head.next
         
