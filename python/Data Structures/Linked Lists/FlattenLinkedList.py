c513d21704a073b24f34294672d52ee35e8eca69
Alan <sinderpl@gmail.com>
2021-08-30 12:04:32 +0100

    flatten linked list improved

--- python/Data Structures/Linked Lists/FlattenLinkedList.py
+++ python/Data Structures/Linked Lists/FlattenLinkedList.py
@@ -36,3 +36,45 @@ class Solution:
         tmp = res
         
         return res
+
+
+
+
+    # Space efficient solution using recursion
+    def flattenRecursive(self, head: 'Node') -> 'Node':
+        if not head:
+            return None
+        
+        while head:
+            nextNode = head.next
+            if head.child:
+                childEnd = self.flattenRecursive(head.child)
+                
+                #  link the beginning of the child list
+                head.next = head.child
+                head.child.prev = head
+                head.child = None
+                
+                # Link the end of the child list 
+                # only if another node exists
+                # Otherwise we return the tail to be linked at a upper level
+                if nextNode:
+                    nextNode.prev = childEnd
+                    childEnd.next = nextNode
+                else:
+                    return childEnd
+                
+            # we make sure to return the last node
+            if not nextNode:
+                return head
+            else:
+                head = nextNode
+        
+        # We should never reach here
+        return None
+    
+    # O(N + M + len(m[x]))runtime where N is each node
+    # Constant space complexity
+    def flatten(self, head: 'Node') -> 'Node':
+        self.flattenRecursive(head)
+        return head
