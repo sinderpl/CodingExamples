93d1612da4d6a2e49b619869103c6ab4657ebda0
Alan <sinderpl@gmail.com>
2021-09-01 19:46:05 +0100

    cleanup

---
+++ python/Data Structures/Linked Lists/RearrangeNodesLinkedList.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        ptr = head.next
+        stack = list()
+        
+        while ptr:
+            stack.append(ptr)
+            ptr = ptr.next
+        
+        end = True
+        while stack:
+            head.next = stack.pop() if end else stack.pop(0)
+            head.next.next = None
+            end = not end
+            
