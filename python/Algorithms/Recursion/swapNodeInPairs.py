f2473c0e0f51f99be31cad5e478f9f0394dbfe27
Alan <sinderpl@gmail.com>
2021-09-06 15:46:42 +0100

    swap node pairs

--- python/Algorithms/Recursion/swapNodeInPairs.py
+++ python/Algorithms/Recursion/swapNodeInPairs.py
@@ -1,1 +1,19 @@
-sd dsds 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        tempNextNext = head.next.next
+        next = head.next
+        tmpHead = head
+        
+        head.next.next = head
+        head = head.next
+        head.next.next = self.swapPairs(tempNextNext)
+        
+        return head
