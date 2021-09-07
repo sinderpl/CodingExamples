00db5ff8e8863b3bf61cfa61214f09b2800fc6da
Alan <sinderpl@gmail.com>
2021-09-07 13:25:38 +0100

    Üpdate reverse linked list with edge case

--- python/Data Structures/Linked Lists/reverseLinkedList.py
+++ python/Data Structures/Linked Lists/reverseLinkedList.py
@@ -5,7 +5,8 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
+        if not head:
+            return head
         prev = None
         
         while head != None:
