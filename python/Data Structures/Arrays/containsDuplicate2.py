9bc59a0345589bf51fc74fe9ad527e9498b9b5c9
Alan <sinderpl@gmail.com>
2021-11-02 18:49:50 +0000

    ccontains duplicate 22

--- python/Data Structures/Arrays/containsDuplicate2.py
+++ python/Data Structures/Arrays/containsDuplicate2.py
@@ -1,4 +1,17 @@
 "Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k"
 
 
-
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        if not nums or len(nums) == 1:
+            return False
+        
+        pairs = dict()
+        
+        for idx, num in enumerate(nums):
+            if num in pairs:
+                if abs(pairs[num] - idx) <= k:
+                    return True
+            pairs[num] = idx
+            
+        return False
