126eac1fff04739dae39a39e18313968da177468
Alan <sinderpl@gmail.com>
2021-11-02 18:48:41 +0000

    ccontains duplicate 1

--- python/Data Structures/Arrays/containsDuplicate1.py
+++ python/Data Structures/Arrays/containsDuplicate1.py
@@ -1,2 +1,15 @@
 "Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct."
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if not nums or len(nums) == 1:
+            return False
+         
+        compliments = set()
+        for num1 in nums:
+            if num1 in compliments:
+                return True
+            else:
+                compliments.add(num1)
+                
+        return False
